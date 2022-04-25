f7c1774aadefc7ecc2692b294c81da48bccf4539
statizarm <statiz.arm@gmail.com>
2022-04-25 14:35:15 +0300

    save

--- div_a/lec5/e.cpp
+++ div_a/lec5/e.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
 
 int main() {
+    std::string input_seq;
+
+    std::cin >> input_seq;
+
+    int64_t res = 0;
+    int64_t count[2] = {0, 1};
+    int64_t it = 1;
+
+    while (it < input_seq.size()) {
+        if (input_seq[it] != input_seq[it - 1]) {
+            count[0] = count[1];
+            count[1] = 1;
+            res += count[0];
+        } else {
+            count[1] += 1;
+            res += count[0];
+        }
+        ++it;
+    }
+
+    res += (count[0] + 1) * count[1];
+
+    std::cout << res << std::endl;
 }
